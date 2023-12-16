64dd48b081015bd86d7d21bfb4f9c4f76146fff3
Davinccx <64002442+Davinccx@users.noreply.github.com>
2023-12-16 17:24:49 +0100

    ventasDashboard funciona

---
+++ Vendify/src/main/java/servlets/Compra.java
@@ -0,0 +1,60 @@
+package servlets;
+
+
+public class Compra {
+    
+    
+    private int id_compra;
+    private String nombre_producto;
+    private String descripcion;
+    private double precio;
+    private String localizacion;
+
+    public Compra() {
+    }
+    
+    
+
+    public int getId_compra() {
+        return id_compra;
+    }
+
+    public void setId_compra(int id_compra) {
+        this.id_compra = id_compra;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+    
+    
+        
+}
--- Vendify/src/main/java/servlets/getVentasFromUser.java
+++ Vendify/src/main/java/servlets/getVentasFromUser.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import com.google.gson.Gson;
+import database.Maquina;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
@@ -10,12 +11,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import static java.lang.System.out;
 import database.Venta;
 import database.User;
+import database.Producto;
 import jakarta.servlet.http.HttpSession;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import logic.Logic;
-
-
 
 public class getVentasFromUser extends HttpServlet {
 
@@ -28,12 +28,13 @@ public class getVentasFromUser extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         try {
-            
+
             System.out.println("-- Get Ventas from User information from DB--");
             response.setContentType("text/html;charset=UTF-8");
             PrintWriter out = response.getWriter();
             HttpSession session = request.getSession();
-            ArrayList<Integer> ventasUsuario = new ArrayList<Integer>();
+            ArrayList<Venta> ventasUsuario = new ArrayList<Venta>();
+            ArrayList<Compra> comprasUsuario = new ArrayList<Compra>();
             if (session != null) {
 
                 String usernameLoggeado = (String) session.getAttribute("username");
@@ -41,7 +42,8 @@ public class getVentasFromUser extends HttpServlet {
                 if (usernameLoggeado != null) {
 
                     ArrayList<Venta> ventas = Logic.getVentasFromDB();
-
+                    ArrayList<Producto> productos = Logic.getProductosFromDB();
+                    ArrayList<Maquina> maquinas = Logic.getMaquinasFromDB();
                     User user = Logic.getUserFromUsername(usernameLoggeado);
                     Integer id_usuario = user.getId();
                     for (int x = 0; x < ventas.size(); x++) {
@@ -51,15 +53,46 @@ public class getVentasFromUser extends HttpServlet {
 
                         if (id_usuario.equals(id_user)) {
 
-                            ventasUsuario.add(venta.getId_venta());
+                            ventasUsuario.add(venta);
 
                         }
 
                     }
-                    
-                    
-                
-                    String jsonVentas = new Gson().toJson(ventasUsuario);
+
+                    for (int i = 0; i < productos.size(); i++) {
+
+                        Producto producto = productos.get(i);
+                        Integer id_producto = producto.getId();
+
+                        for (int j = 0; j < ventasUsuario.size(); j++) {
+
+                            Venta venta = ventasUsuario.get(j);
+
+                            for (Maquina maquina : maquinas) {
+                                if (venta.getId_producto() == id_producto && producto.getId_maquina() == maquina.getId()) {
+
+                                    Integer id_compra = venta.getId_venta();
+                                    String nombre_producto = producto.getNombre();
+                                    String desc_producto = producto.getDescripcion();
+                                    double precio = producto.getPrecio();
+
+                                    Compra compraUser = new Compra();
+                                    compraUser.setId_compra(id_compra);
+                                    compraUser.setNombre_producto(nombre_producto);
+                                    compraUser.setDescripcion(desc_producto);
+                                    compraUser.setPrecio(precio);
+                                    compraUser.setLocalizacion(maquina.getLocation());
+
+                                    comprasUsuario.add(compraUser);
+
+                                }
+                            }
+
+                        }
+
+                    }
+
+                    String jsonVentas = new Gson().toJson(comprasUsuario);
                     System.out.println("JSON Values=> " + jsonVentas);
                     out.println(jsonVentas);
                     out.close();
